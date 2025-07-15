a2fe188c5d4eaa0ebc37fb6d311bed7a49f51e2d
AdityaHood <adityahood13@gmail.com>
2025-07-16 00:43:12 +0530

    BACKEND PROJECT

--- Readme.md
+++ Readme.md
@@ -1,1 +1,15 @@
 # A backend Exercise Folder
+
+# Aggregation:
+
+# Paginate:
+
+# Bcrypt
+
+# JWT: ,explain bear token in it
+
+# It is impossible to encrypt the password or text using bcrypt directly,we have to some middlewares for it.
+
+# Middlewares provides some hooks for it like 'pre'
+
+# Pre Hook
---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// Accepting the data from different sources in a json format but make sure to put a limit on it or else it may crash
+//This data is coming from files
+app.use(express.json({ limit: "16kb" }));
+
+//Data coming from url's is encoded so we have to use specific configuration
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
